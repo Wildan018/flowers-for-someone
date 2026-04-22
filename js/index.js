b565b868e63c298f8bc0148897e592109dd4bdf5
wildan <152429993+Wildan018@users.noreply.github.com>
2026-04-22 22:20:34 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Ayuni Agustin 
+Rahayu`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
